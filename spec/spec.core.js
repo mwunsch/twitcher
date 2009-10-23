6f6abd11d8cb4fa4be5426897be4da7120cb6daf
Mark Wunsch <mark@markwunsch.com>
2009-10-23 13:05:12 -0400

    testing parameter passing

--- spec/spec.core.js
+++ spec/spec.core.js
@@ -1,29 +1,49 @@
 
 describe 'Twitcher'
-  before
-    search = Twitcher("from:markwunsch",{'lang':'en','rpp':'20','show_user':false, 'foo':'bar'})
+  before_each
+    search = Twitcher("from:markwunsch")
   end
   
-  it 'should return a Twitcher object'
+  it 'should create a Twitcher object'
     search.should.be_an Object
   end
   
   it 'should remember the query string'
-    search.setup.query_string.should.be "from:markwunsch"
+    search.query_string.should.be "from:markwunsch"
   end
   
   it 'should compose/encode a Twitter API url'
     search.compose_url().should.be "http://search.twitter.com/search.json?q=from%3Amarkwunsch"
   end
   
-  it 'should accept parameters for Search API'
-    search.setup.parameters.lang.should.be 'en'
-    search.setup.parameters.rpp.should.be '20'
-    search.setup.parameters.show_user.should.be 'false'
+  it 'should have no params'
+    search.parameters.should.be_empty
   end
   
-  it 'should not allow params not defined by Search API'
-    search.setup.parameters.foo.should_not.be 'bar'
+  describe 'with params'
+    before_each
+      search = Twitcher("foobar",{'lang':'en','rpp':'20','show_user':false, 'foo':'bar'})
+    end
+    
+    it 'should setup'
+      search.should.be_an Object
+      search.query_string.should.be "foobar"
+    end
+
+    it 'should accept parameters for Search API'
+      search.parameters.lang.should.be 'en'
+      search.parameters.rpp.should.be '20'
+      search.parameters.show_user.should.be 'false'
+      search.parameters.show_user.should_not.be false
+    end
+
+    it 'should not allow params not defined by Search API'
+      search.parameters.foo.should_not.be 'bar'
+    end
+    
+    it 'should compose/encode a Twitter API url'
+      
+    end
   end
   
 end
